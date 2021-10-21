9f0dc87696a82eb749661d9f62a6674c80963b1d
kyofuuu <91661237+kyofuuu@users.noreply.github.com>
2021-10-21 10:22:29 +0300

    Add files via upload

---
+++ laba23_5a.cpp
@@ -0,0 +1,8 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main() {
+	char* some_memory = (char*)0;
+	printf("A read from null %sn", some_memory);
+	sprintf(some_memory, "A write to nulln");
+	exit(EXIT_SUCCESS);
+}
